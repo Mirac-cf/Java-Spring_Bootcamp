f7402009d76d22f0aa0b7b903c08d70ac0cbfe7d
Mirac <miracciftci.cs@gmail.com>
2022-12-30 13:47:38 +0300

    Update BrandService.java

--- Lessons/rentACar/src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
+++ Lessons/rentACar/src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
@@ -8,4 +8,5 @@ import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
 public interface BrandService {
 	List<GetAllBrandsResponse> getAll();
 	void add(CreateBrandRequest createBrandRequest);
+	//heloo
 }
